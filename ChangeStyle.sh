b8eb62657e0ffdc62874cc6cd712569ba12bcd32
Julio Elementary <juliocesar.mr@protonmail.com>
2020-03-15 17:45:30 -0600

    Agregamos opción a elegir si es laptop, agregando configuración para mostrar nivel de batería en i3blocks

--- ChangeStyle.sh
+++ ChangeStyle.sh
@@ -1,6 +1,14 @@
-read -n1 -p "Light or Dark" doit 
+#!/bin/bash
+
+read -n1 -p "Light or Dark [l|d]: " doit 
 case $doit in  
   l|L) cp i3/config_light $HOME/.config/i3/config && cp VisualStudioCode/light_settings.json $HOME/.config/Code/User/settings.json;; 
   d|D) cp i3/config_dark $HOME/.config/i3/config ;; 
   *) echo dont know ;; 
+esac
+
+read -n1 -p "Laptop? [y|n]: " doit
+case $doit in
+	y|Y) cat i3/LaptopAdd >> $HOME/.config/i3/i3blocks.conf ;;
+	*) ;;
 esac
